bd5179f2206edf8a7d2a436aea9836f12023684d
jamelablack <jamela.black@gmail.com>
2015-03-06 09:06:53 -0500

    added video show page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,7 +2,10 @@ class VideosController < ApplicationController
 
 	def index
 		@videos = Video.all
+	end
 
+	def show
+		@video = Video.find(params[:id])
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Myflix::Application.routes.draw do
+	root to: 'videos#index'
 	get '/home', to: 'videos#index'
   get 'ui(/:action)', controller: 'ui'
+  
+  resources :videos, only: [:show]
 
 end
