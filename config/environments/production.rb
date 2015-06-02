e865997114a9ffeba4bb50d33ba417c4363e25ce
jamelablack <jamela.black@gmail.com>
2015-06-02 15:09:17 -0400

    add mailgun/heroku settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,4 +17,16 @@ Myflix::Application.configure do
   config.i18n.fallbacks = true
 
   config.active_support.deprecation = :notify
+
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for mailgun
+    ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'https://aqueous-coast-5067.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 end
