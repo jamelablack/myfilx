34d12d99d5afb8ba0d2b955ae17a2b11573cfea8
Jamela Black <jamela.black@gmail.com>
2015-08-23 19:18:03 -0400

    Refactor UserSignup for clarity

--- app/services/user_signup.rb
+++ app/services/user_signup.rb
@@ -1,47 +1,59 @@
 class UserSignup
 
-  attr_reader :error_message
+  attr_accessor :error_message, :user
 
   def initialize(user)
     @user = user
   end
 
   def sign_up(stripe_token, invitation_token)
-    if @user.valid?
-      customer = StripeWrapper::Customer.create(
-        :user => @user,
-        :card => stripe_token,
-      )
-      if customer.successful?
-        @user.customer_token = customer.customer_token
-        @user.save
-        handle_invitations(invitation_token)
-        AppMailer.delay.send_welcome_email(@user.id)
-        @status = :success
-        self
-      else
-        @status = :failed
-        @error_message = customer.error_message
-        self
-      end
+    self.stripe_token = stripe_token
+    self.invitation_token = invitation_token
+
+    handle_invalid and return self if user.invalid?
+
+    if customer.successful?
+      user.customer_token = customer.customer_token
+      user.save
+      handle_invitations(invitation_token)
+      AppMailer.delay.send_welcome_email(user.id)
+      self.status = :success
     else
-      @status = :failed
-      @error_message = "Your user info is invalid. Please see errors below."
-      self
+      handle_failure(customer.error_message)
     end
+
+    self
   end
 
   def successful?
-    @status == :success
+    status == :success
   end
 
   private
 
+  attr_accessor :status, :customer, :stripe_token, :invitation_token
+
+  def customer
+    @customer ||= StripeWrapper::Customer.create(
+      :user => user,
+      :card => stripe_token,
+    )
+  end
+
+  def handle_failure(message)
+    self.status = :failed
+    self.error_message = message
+  end
+
+  def handle_invalid
+    handle_failure("Your user info is invalid. Please see errors below.")
+  end
+
   def handle_invitations(invitation_token)
     if invitation_token.present?
       invitation = Invitation.where(token: invitation_token).first
-      @user.follow(invitation.inviter)
-      invitation.inviter.follow(@user)
+      user.follow(invitation.inviter)
+      invitation.inviter.follow(user)
       invitation.update_column(:token, nil)
     end
   end
