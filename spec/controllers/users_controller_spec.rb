904543bf5194c8d7b15c22da170f3e33e85b49df
Jamela Black <jamela.black@gmail.com>
2015-08-24 17:34:33 -0400

    White space fix

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,76 +2,76 @@ require 'spec_helper'
 
 
 describe UsersController do
-	describe "GET new" do
-		it "sets @user" do
-			get :new
-			expect(assigns(:user)).to be_instance_of(User)
-			end
-		end
-	describe "POST create" do
-		context "successful user sign up" do
+  describe "GET new" do
+    it "sets @user" do
+      get :new
+      expect(assigns(:user)).to be_instance_of(User)
+    end
+  end
+  describe "POST create" do
+    context "successful user sign up" do
 
-			it "redirects to th sign on page" do
-				result = double(:sign_up_result, successful?: true)
-				UserSignup.any_instance.should_receive(:sign_up).and_return(result)
+      it "redirects to th sign on page" do
+        result = double(:sign_up_result, successful?: true)
+        UserSignup.any_instance.should_receive(:sign_up).and_return(result)
 
-				post :create, user: Fabricate.attributes_for(:user)
-				expect(response).to redirect_to sign_in_path
-			end
-		end
+        post :create, user: Fabricate.attributes_for(:user)
+        expect(response).to redirect_to sign_in_path
+      end
+    end
 
-		context "failed user sign up" do
-			it "renders the new template" do
-				result = double(:sign_up_result, successful?: false, error_message: "This is the error message")
-				UserSignup.any_instance.should_receive(:sign_up).and_return(result)
-				post :create, user: Fabricate.attributes_for(:user), stripeToken: '1234456'
-				expect(response).to render_template(:new)
-			end
+    context "failed user sign up" do
+      it "renders the new template" do
+        result = double(:sign_up_result, successful?: false, error_message: "This is the error message")
+        UserSignup.any_instance.should_receive(:sign_up).and_return(result)
+        post :create, user: Fabricate.attributes_for(:user), stripeToken: '1234456'
+        expect(response).to render_template(:new)
+      end
 
-			it "sets the flash error message" do
-				result = double(:sign_up_result, successful?: false, error_message: "This is the error message")
-				UserSignup.any_instance.should_receive(:sign_up).and_return(result)
-				post :create, user: Fabricate.attributes_for(:user), stripeToken: '1234456'
-				expect(flash[:error]).to eq("This is the error message")
-			end
-		end
-	end
+      it "sets the flash error message" do
+        result = double(:sign_up_result, successful?: false, error_message: "This is the error message")
+        UserSignup.any_instance.should_receive(:sign_up).and_return(result)
+        post :create, user: Fabricate.attributes_for(:user), stripeToken: '1234456'
+        expect(flash[:error]).to eq("This is the error message")
+      end
+    end
+  end
 
-	describe  "GET show" do
-		it_behaves_like "requires sign in" do
-			let(:action) { get :show, id: 3 }
-		end
+  describe  "GET show" do
+    it_behaves_like "requires sign in" do
+      let(:action) { get :show, id: 3 }
+    end
 
-		it "sets @user" do
-			set_current_user
-			alice = Fabricate(:user)
-			get :show, id: alice.id
-			expect(assigns(:user)).to eq(alice)
-		end
-	end
+    it "sets @user" do
+      set_current_user
+      alice = Fabricate(:user)
+      get :show, id: alice.id
+      expect(assigns(:user)).to eq(alice)
+    end
+  end
 
-	describe "GET new_with_invitation_token" do
-		it "render the :new view template" do
-			invitation = Fabricate(:invitation)
-			get :new_with_invitation_token, token: invitation.token
-			expect(response).to render_template :new
-		end
+  describe "GET new_with_invitation_token" do
+    it "render the :new view template" do
+      invitation = Fabricate(:invitation)
+      get :new_with_invitation_token, token: invitation.token
+      expect(response).to render_template :new
+    end
 
-		it "sets @user with recipient's email" do
-			invitation = Fabricate(:invitation)
-			get :new_with_invitation_token, token: invitation.token
-			expect(assigns(:user).email).to eq(invitation.recipient_email)
-		end
+    it "sets @user with recipient's email" do
+      invitation = Fabricate(:invitation)
+      get :new_with_invitation_token, token: invitation.token
+      expect(assigns(:user).email).to eq(invitation.recipient_email)
+    end
 
-		it "sets @invitation_token" do
-			invitation = Fabricate(:invitation)
-			get :new_with_invitation_token, token: invitation.token
-			expect(assigns(:invitation_token)).to eq(invitation.token)
-		end
+    it "sets @invitation_token" do
+      invitation = Fabricate(:invitation)
+      get :new_with_invitation_token, token: invitation.token
+      expect(assigns(:invitation_token)).to eq(invitation.token)
+    end
 
-		it "redirects to expired token page for invalid tokens" do
-			get :new_with_invitation_token, token: 'asdffdhh'
-			expect(response).to redirect_to expired_token_path
-		end
-	end
+    it "redirects to expired token page for invalid tokens" do
+      get :new_with_invitation_token, token: 'asdffdhh'
+      expect(response).to redirect_to expired_token_path
+    end
+  end
 end
