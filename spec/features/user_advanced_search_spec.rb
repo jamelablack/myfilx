9ff2a81825c93a592ef6173c88b312f2dff73136
Jamela Black <jamela.black@gmail.com>
2015-09-12 23:47:42 -0400

    Shared header fix

--- spec/features/user_advanced_search_spec.rb
+++ spec/features/user_advanced_search_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
+feature "User interacts with advanced search", :elasticsearch do
+  background do
+    Fabricate(:video, title: "Home Alone")
+    Fabricate(:video, title: "Home Alone 2: Kevin is alone in NYC")
+    Fabricate(:vidoe, title: "Home is Where the Heart is")
+    Fabricate(:video, title: "Love Actually", description: "Love and the holidays")
+
+    refersh_index
+    sign_in
+    click_on "Advanced Search"
+  end
+
+  scenario "user searches with title" do
+    within(".advanced_search") do
+      fill_in "query" with: "Home Alone"
+      click_button "Search"
+    end
+
+    expect(page).to have_content("2 videos found")
+    expect(page).to have_content("Home Alone")
+    expect(page).to have_content("Home Alone 2: Kevin is alone in NYC")
+    expect(page).to have_no_content("Home is Where the Heart is")
+  end
+
+  scenario "user searches with the title and description" do
+    within(".advanced_search") do
+      fill_in "query", with: "love and the holidays"
+      click_button "Search"
+    end
+
+    expect(page).to have_content("Love Acutally")
+    expect(page).to have_no_content("Home")
+  end
+
+  def refresh_index
+    Video.import
+    Video.__elasticsearch__.refresh_index
+  end
+
   background do
     home_alone = Fabricate(:video, title: "Hame Alone 1")
     home_alone_2 = Fabricate(:video, title: "Home Alone 2")
@@ -42,3 +81,4 @@ require 'spec_helper'
     expect(page).to have_no_content "Home Alone 1"
     expect(page).to have_no_content "Home Alone 2"
   end
+end
