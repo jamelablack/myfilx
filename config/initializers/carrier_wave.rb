dfe94af04b8c0dc35e92ca7f64c576457b557f9f
Jamela Black <jamela.black@gmail.com>
2015-09-24 10:12:09 -0400

    Carrierwave update

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,14 @@
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
-    config.storage = :fog
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              ENV['provider'],                        # required
-    aws_access_key_id:     ENV['aws_access_key_id'],                        # required
-    aws_secret_access_key: ENV['aws_secret_access_key'],                        # required
-  }
-  config.fog_directory  = 'name_of_directory'                          # required
+    config.storage    = :aws
+    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
+    config.aws_acl    = 'public-read'
+
+    config.aws_credentials = {
+      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      region:            ENV.fetch('AWS_REGION') # Required
+    }
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
